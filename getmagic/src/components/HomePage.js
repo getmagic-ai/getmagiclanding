6018dbd23f33a18a8e3705fa3405a471790a7c5b
andylnkd <dhalwani1@gmail.com>
2022-08-30 04:39:29 +0000

    added in tailwind and cleant up the old css files

---
+++ getmagic/src/components/HomePage.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react'
+
+export default function HomePage() {
+
+
+    return (
+        <div class = "p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+            <main class="">
+
+                'Hey';
+
+            </main>
+
+        </div>
+
+
+    )
+}
+
