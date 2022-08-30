a400b110949cdfed7fd3f11f63c102cd15e4c549
andylnkd <dhalwani1@gmail.com>
2022-08-30 18:21:39 +0000

    Aug 30 committs for tailwind and clean up

--- getmagic/src/App.js
+++ getmagic/src/App.js
@@ -4,7 +4,10 @@ import HomePage from './components/HomePage';
 
 function App() {
   return (
-      <HomePage />  
+    <main class="bg-green-300"> 
+      <HomePage />
+    </main>
+
   );
 }
 
--- getmagic/src/components/HomePage.js
+++ getmagic/src/components/HomePage.js
@@ -4,8 +4,8 @@ export default function HomePage() {
 
 
     return (
-        <div class = "p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-            <main class="height-10 width-10">
+        <div class = "p-6 max-w-sm mx-auto absolute bottom-10 top-50 left-50 right-50 rounded-xl shadow-lg flex items-center space-x-4 bg-red-400">
+            <main class="font-extrabold ring-emerald-700 ">
                 'Hey';
 
             </main>
