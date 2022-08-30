e2e7c4eb35702698edd02e696974d3a6320a03f0
andylnkd <dhalwani1@gmail.com>
2022-08-30 05:03:26 +0000

    added some more stuff to start tailwind, wasn't working

--- getmagic/src/components/HomePage.js
+++ getmagic/src/components/HomePage.js
@@ -5,8 +5,7 @@ export default function HomePage() {
 
     return (
         <div class = "p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-            <main class="">
-
+            <main class="height-10 width-10">
                 'Hey';
 
             </main>
