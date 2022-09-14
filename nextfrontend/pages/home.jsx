3d5f30dd3e8c9d2f09eafff3f3841eeedbf3cc51
andylnkd <dhalwani1@gmail.com>
2022-09-14 00:51:15 +0000

    updates

--- nextfrontend/pages/home.jsx
+++ nextfrontend/pages/home.jsx
@@ -1,13 +1,12 @@
-export default function LandingPage(){
+import Image from "next/image";
 
-  return(
-
-   <>
-   
-    <img src='./../assets/influencer_card3.png' />
-   
-   </>
-
-  )
-
+export default function LandingPage() {
+  return (
+    <section className="flex-1 flex-row justify-center gap-2 m-2 p-2">
+      <div className="flex p-2 m-2 bg-amber-900">Hey</div>
+      <div className="flex p-2 m-2 bg-cyan-900">Hey there</div>
+      <div className="flex p-2 m-2 bg-blue-900">Hey how are you</div>
+      <div className="flex p-2 m-2 bg-green-900">Hey</div>
+    </section>
+  );
 }
