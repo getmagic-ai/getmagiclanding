373e5a742de95f4bd01df756cff840139b0ac28b
andylnkd <dhalwani1@gmail.com>
2022-09-14 01:26:43 +0000

    Made the column change and did the trick on equal spaced columns

--- nextfrontend/pages/home.jsx
+++ nextfrontend/pages/home.jsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 
 export default function LandingPage() {
+    const colCount = 3;
+  const classStr=`columns-${colCount} w-auto  justify-center gap-2 m-2 mx-14 p-2`
+  
   return (
-    <section className="flex-1 flex-row justify-center gap-2 m-2 p-2">
-      <div className="flex p-2 m-2 bg-amber-900">Hey</div>
-      <div className="flex p-2 m-2 bg-cyan-900">Hey there</div>
-      <div className="flex p-2 m-2 bg-blue-900">Hey how are you</div>
-      <div className="flex p-2 m-2 bg-green-900">Hey</div>
+    <section className="flex flex-row items-center justify-center pt-5 mx-5">
+      <div className="flex w-15 p-2 m-2 bg-amber-900">Hey</div>
+      <div className="flex w-35 p-2 m-2 bg-cyan-900">Hey there</div>
+      <div className="flex w-25 p-2 m-2 bg-blue-900">Hey how are you</div>
+      <div className="flex w-15 p-2 m-2 bg-green-900">Hey</div>
     </section>
   );
 }
